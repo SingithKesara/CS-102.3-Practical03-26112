312fe4e7372393332086eda5eddef984735d2453
SingithKesara <114977489+SingithKesara@users.noreply.github.com>
2023-06-28 14:16:10 +0530

    Update 2.c

--- 2.c
+++ 2.c
@@ -1,37 +1,29 @@
 #include <stdio.h>
 
 int main() {
-    int num1, num2, num3;
-    int largest, smallest;
+  int num1, num2, num3;
 
-    printf("Enter the first number: ");
-    scanf("%d", &num1);
+  printf("Enter three integer numbers: ");
+  scanf("%d %d %d", &num1, &num2, &num3);
 
-    printf("Enter the second number: ");
-    scanf("%d", &num2);
+  int largest = num1;
+  if (num2 > largest) {
+    largest = num2;
+  }
+  if (num3 > largest) {
+    largest = num3;
+  }
 
-    printf("Enter the third number: ");
-    scanf("%d", &num3);
+  int smallest = num1;
+  if (num2 < smallest) {
+    smallest = num2;
+  }
+  if (num3 < smallest) {
+    smallest = num3;
+  }
 
-    largest = num1;
-    smallest = num1;
+  printf("The largest number is: %d\n", largest);
+  printf("The smallest number is: %d\n", smallest);
 
-    if (num2 > largest) {
-        largest = num2;
-    }
-    if (num2 < smallest) {
-        smallest = num2;
-    }
-
-    if (num3 > largest) {
-        largest = num3;
-    }
-    if (num3 < smallest) {
-        smallest = num3;
-    }
-
-    printf("The largest number is: %d\n", largest);
-    printf("The smallest number is: %d\n", smallest);
-
-    return 0;
+  return 0;
 }
